7e9190239aa7b8fe64e0b6158b05eb9feff76258
Quang21067 <vunhatquang5a@gmail.com>
2026-09-09 03:04:16 +0700

    chore: remove local backend from repository

--- backend/app/core/authorization.py
+++
@@ -1,7 +0,0 @@
-from app.core.config import settings
-from app.model.user import User
-
-
-def is_admin_user(user: User) -> bool:
-    """Return whether the user has the application's administrator privileges."""
-    return user.id == 1 or user.username.lower() == settings.admin_username.lower()
