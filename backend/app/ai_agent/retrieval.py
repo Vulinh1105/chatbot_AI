7c73ae91692d5dd7f8d1d374b570bf552e6f2500
phuchapc06-lab <phuchapc06@gmail.com>
2026-09-09 10:47:47 +0700

    fix

---
+++ backend/app/ai_agent/retrieval.py
@@ -0,0 +1,56 @@
+
+from pathlib import Path
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+FAISS_PATH = Path(__file__).resolve().parent.parent.parent / 'faiss_index'
+embedder = OpenAIEmbeddings(
+    model='text-embedding-3-large'
+)
+
+# lấy phaanf embedding ở T12
+db = FAISS.load_local(
+    FAISS_PATH,
+    embedder,
+    allow_dangerous_deserialization=True
+)
+
+retriever=db.as_retriever(
+    search_type='similarity_score_threshold',
+    search_kwargs={'k': 3, 'score_threshold': 0.2}
+)
+print('done topK + score')
+
+
+# Hàm Retrieval
+def retrieve(query):
+    results = retriever.invoke(query)
+    return results
+
+
+if __name__ == '__main__':
+    while True:
+        query = input('Question (type "exit" to quit): ')
+
+        if query == 'exit':
+            print('program exited.')
+            break
+        if not query.strip():
+            continue
+
+
+        results = retrieve(query)
+
+        print(f'\nFind {len(results)} chunks:\n')
+
+        for i, doc in enumerate(results, start=1):
+            print(f'--- Result {i} ---')
+            print('Content:', doc.page_content)
+            print('Metadata:', doc.metadata)
+
+print('số vector trg FAISS: ', db.index.ntotal)
